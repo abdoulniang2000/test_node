a7164c6b52805365f4fc39ef15ad8d9e2b74110e
Abdoul Niang <abdoilniang00@gmail.com>
2024-08-16 20:23:07 +0000

    mes premieres codes

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const http = require('http');
-const server = http.createServer((req, res)=> {
-    res.end("Hello My name is Abdoul Niang");
-});
+const express = require('express');
+let etudiants = require('./data')
 
-server.listen(3000, () =>{
+
+//const server = http.createServer((req, res)=> {
+  //  res.end("Hello My name is Abdoul Niang");
+//}); 
+
+const app = express();
+app.get('/etudiants/:id', (req, res) =>{
+    const id = req.params.id;
+    res.send(`Etudiant numero ${id}`);
+})
+
+app.get('/etudiants', (req, res) =>{
+    const nbrEtudiants = etudiants.length;
+    res.send(` Il ya ${nbrEtudiants} etudiants`);
+})
+
+app.listen(3000, () =>{
     console.log("serveur demare sur le port 3000");
 });
---
+++ data.js
@@ -0,0 +1,24 @@
+ const etudiants = [
+    {
+        id: 1,
+        name: "Abdoul",
+    },
+    {
+        id: 2,
+        name: "Farid",
+    },
+    {
+        id: 3,
+        name: "Said",
+    },
+    {
+        id: 4,
+        name: "Hadija",
+    },
+    {
+        id: 5,
+        name: "saydou",
+    },
+
+]
+module.exports = etudiants;
